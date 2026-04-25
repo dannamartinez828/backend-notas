240feab75fb04faa874c3c6fee98e3730f632e71
Dana Paola Martínez Oliva <danna paolamar@gmail.com>
2026-04-25 06:19:15 -0500

    rutas completas

--- app.js
+++ app.js
@@ -88,35 +88,49 @@ app.post('/notas', async (req, res) => {
   try {
     const { cedula, materia, nota1, nota2, nota3, nota4 } = req.body;
 
+    // buscar estudiante
     const estudiante = await pool.query(
       'SELECT * FROM estudiantes WHERE cedula=$1',
       [cedula]
     );
 
     if (estudiante.rows.length === 0) {
-      return res.status(404).json({ mensaje: 'No existe estudiante' });
+      return res.status(404).json({
+        mensaje: "Estudiante no existe"
+      });
     }
 
-    const id = estudiante.rows[0].id;
+    const estudiante_id = estudiante.rows[0].id;
 
     const definitiva =
       (Number(nota1) +
-        Number(nota2) +
-        Number(nota3) +
-        Number(nota4)) / 4;
+       Number(nota2) +
+       Number(nota3) +
+       Number(nota4)) / 4;
 
     const result = await pool.query(
       `INSERT INTO notas
       (estudiante_id,materia,nota1,nota2,nota3,nota4,definitiva)
       VALUES($1,$2,$3,$4,$5,$6,$7)
       RETURNING *`,
-      [id, materia, nota1, nota2, nota3, nota4, definitiva]
+      [
+        estudiante_id,
+        materia,
+        nota1,
+        nota2,
+        nota3,
+        nota4,
+        definitiva
+      ]
     );
 
     res.json(result.rows[0]);
 
   } catch (error) {
-    res.status(500).json(error);
+    console.log(error);
+    res.status(500).json({
+      error: error.message
+    });
   }
 });
 
